81dfe8cdeaec586aa36a1928c53ff24c9b423f70
Violet Vivirand <purpleslovemail@gmail.com>
2022-09-05 17:15:06 +0800

    Add feature to generate s3 access log report

--- main.py
+++ main.py
@@ -99,6 +99,68 @@ def generate_sessionrecording_report(bucket_name_sessionrecording, stack_name, f
         for row in rows:
             spamwriter.writerow(row)
 
+def generate_s3log_report(database, table, users_detail, datestart=None, dateend=None):
+    import awswrangler as wr
+    import pandas as pd
+
+    # Generate user name hash table for replacing
+    hash_username = {hashlib.sha256(str.encode(user_detail['UserName'])).hexdigest():user_detail['UserName'] for user_detail in users_detail}
+    # Generate user detail DataFrame for merging DataFrames
+    users_df = pd.DataFrame(users_detail)
+    users_df.drop("Hash", axis=1, inplace=True)
+
+    if not datestart:
+        # Search data before dateend
+        date_between_sql = f"""parse_datetime(requestdatetime, 'dd/MMM/yyyy:HH:mm:ss Z') <= parse_datetime(
+            '{dateend}',
+            'yyyy-MM-dd HH:mm:ss Z'
+        )"""
+    elif not dateend:
+        # Search data after datestart
+        date_between_sql = f"""parse_datetime(requestdatetime, 'dd/MMM/yyyy:HH:mm:ss Z') >= parse_datetime(
+            '{datestart}',
+            'yyyy-MM-dd HH:mm:ss Z'
+        )"""
+    else:
+        # Search data between datestart and dateend
+        date_between_sql = f"""parse_datetime(requestdatetime, 'dd/MMM/yyyy:HH:mm:ss Z') BETWEEN parse_datetime(
+            '{datestart}',
+            'yyyy-MM-dd HH:mm:ss Z'
+        ) AND parse_datetime(
+            '{dateend}',
+            'yyyy-MM-dd HH:mm:ss Z'
+        )"""
+
+    # Prepare SQL query for reading only PUTOBJECT and BATCH.DELETE operationos
+    SQL = f"""SELECT *
+    FROM "{database}"."{table}"
+    WHERE {date_between_sql}
+        AND operation IN (
+		    'REST.PUT.OBJECT',
+		    'REST.COPY.OBJECT',
+		    'REST.COPY.OBJECT_GET',
+		    'REST.BATCH.DELETE'
+	    );"""
+
+    # Read access log via Data Wrangler as DataFrame
+    df = wr.athena.read_sql_query(SQL, database=database)
+
+    # Regex, group 1: user/userpool/, group 2: username hash
+    r = r"(^user\/userpool\/)(\w+)(\/){1}"
+    # Create a new userpool username column, extract username hash from 'key' column
+    # and try to match with username hash table (hash_username)
+    df.loc[:, 'UserName'] = df.loc[:, 'key'].str.extract(r).iloc[:, 1].replace(hash_username)
+    # Merge with the rest user details
+    df = pd.merge(df, users_df, how='left', on=['UserName'])
+    # Convert the requestdatetime as datetime for sorting
+    df.loc[:, 'requestdatetime'] = pd.to_datetime(df.loc[:, 'requestdatetime'], format="%d/%b/%Y:%H:%M:%S %z")
+
+    # Export
+    df_export = df.loc[:, ['key', 'requestdatetime', 'UserName', 'FirstName', 'LastName']]\
+        .sort_values(by=['requestdatetime'], ascending=False)\
+        .reset_index(drop=True)
+    df_export.to_csv('report_s3log.csv')
+
 def export_homefolder_report(bucket=None):
     # Get clients
     s3 = boto3.client('s3')
@@ -126,16 +188,19 @@ def export_homefolder_report(bucket=None):
 def export_sessionrecording_report(bucket=None, stack=None, fleet=None):
     if not(bucket):
         print("Please provide Bucket name for storing session recording files.")
+        print("by adding \"--bucket\" option.")
         print("Exit...")
         os.exit(1)
     
     if not(stack):
         print("Please provide Stack name with session recording feature enabled.")
+        print("by adding \"--stack\" option.")
         print("Exit...")
         os.exit(1)
     
     if not(fleet):
         print("Please provide Fleet name with session recording feature enabled.")
+        print("by adding \"--fleet\" option.")
         print("Exit...")
         os.exit(1)
 
@@ -151,10 +216,44 @@ def export_sessionrecording_report(bucket=None, stack=None, fleet=None):
         fleet_name=fleet,
         users_detail=users_detail)
 
+def export_s3log_report(database, table, datestart=None, dateend=None):
+    if not database:
+        print("Please provide Glue Data Catalog database name for storing S3 access logs")
+        print("by adding \"--database\" option.")
+        print("Exit...")
+        os.exit(1)
+
+    if not table:
+        print("Please provide Glue Data Catalog table name for storing S3 access logs")
+        print("by adding \"--table\" option.")
+        print("Exit...")
+        os.exit(1)
+
+    if not datestart and not dateend:
+        print("Please provide at least one date reference in ISO format for searcing data")
+        print("by adding \"--datestart\" or \"--dateend\" option.")
+
+    # Get client
+    appstream = boto3.client('appstream')
+
+    # Get users' information from User Pool
+    users_detail = get_users_detail(appstream)
+
+    generate_s3log_report(
+        database=database,
+        table=table,
+        users_detail=users_detail,
+        datestart=datestart,
+        dateend=dateend
+    )
+
+
+
 def main():
     fire.Fire({
         'home-folder': export_homefolder_report,
         'session-recording': export_sessionrecording_report,
+        's3-accesslog': export_s3log_report,
     })
 
 if __name__ == "__main__":
